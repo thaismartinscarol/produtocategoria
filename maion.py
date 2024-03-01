4e9817c3be65f4bec6c0ef33ff31d202b25824e8
ALUNO <alunoifsp@ifsp.com>
2024-02-29 22:34:31 -0300

    Criação das classes POPO

--- maion.py
+++ maion.py
@@ -1,7 +1,7 @@
 import uuid
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy import create_engine,select
+from sqlalchemy.orm import DeclarativeBase, relationship, Session
 from sqlalchemy import Column, Uuid, String, DateTime, func, DECIMAL,Integer, BOOLEAN,ForeignKey
 
 
@@ -13,7 +13,7 @@ motor = create_engine('sqlite+pysqlite:///banco_de_dados.sqlite',
 class Base(DeclarativeBase):
     pass
 
-class DataMixin():
+class DataMixin:
     dta_cadastro = Column(DateTime,server_default=func.now(),  nullable=False)
 
     dtaatualizacao = Column(DateTime, onupdate=func.now(), default=func.now(),nullable=False)
@@ -26,7 +26,7 @@ class Categoria(Base, DataMixin):
     lista_de_produtos = relationship("Produto", back_populates="categoria",
                                  cascade="all, delete-orphan", lazy="selectin")
 
-class Produtos(Base, DataMixin):
+class Produto(Base, DataMixin):
         __tablename__ = 'tb_produtos'
         id = Column(Uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
         nome = Column(String(256), nullable=False)
@@ -34,3 +34,23 @@ class Produtos(Base, DataMixin):
         estoque =Column (Integer, default=0)
         ativo =Column(BOOLEAN,default=True)
         categoria_id =Column(Uuid(as_uuid=True), ForeignKey('tbl_categorias.id'))
+
+
+cat = Categoria()
+cat.nome = "Bebidas"
+
+prod = Produto()
+prod.nome = "Coca cola zero,2l"
+prod.ativo = True
+prod. preco = 9.50
+prod. estoque = 100
+prod .categoria = cat
+
+#with Session(motor) as sessao:
+   # sessao.add(prod)
+    #sessao.commit()
+
+#with Session(motor) as sessao:
+  #categoria = sessao.execute(select(Categoria). where(Categoria.nome == "Bebidas")). scalar()
+#for categoria in categoria:
+   # print(f"A categoria {categoria.nome} tem {len(categoria.lista_de_produtos)} produtos")
